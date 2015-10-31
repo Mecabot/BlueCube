3236c1242563cf830c994d51eb742a3eead0a753
Adam Reed <adam@secretcode.ninja>
2015-10-31 15:12:40 +1100

    Finish removing unneeded items and implement the StatusBar plugin so that text is correctly shown.

--- www/js/index.js
+++ www/js/index.js
@@ -17,19 +17,14 @@ var app = {
     //
     // The scope of 'this' is the event.
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+		
+	  StatusBar.overlaysWebView( false );
+	  StatusBar.backgroundColorByHexString('#ffffff');
+	  StatusBar.styleDefault();
+
+
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
 };
 
 app.initialize();
