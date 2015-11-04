d90d804f0d08d9e2da737824bf2578334c2e5dcf
Adam Reed <adam@secretcode.ninja>
2015-11-04 22:01:53 +1100

    Add a Angular Directive for "uicolorpicker" which we use for adding the colour picker to the page.

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'BlueCube' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('BlueCube', ['ionic', 'BlueCube.controllers', 'ngCordova'])
+angular.module('BlueCube', ['ionic', 'BlueCube.controllers', 'ngCordova', 'BlueCube.directives'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
