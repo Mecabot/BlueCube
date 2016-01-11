3cabab7ca289cf53d9979e29f7fc48be5f940cea
Adam Reed <adam@secretcode.ninja>
2016-01-11 12:41:11 +1100

    Comment Facesweep, and slightly reorg some code so that it flows better

--- Arduino/BlueCube/FaceSweep.cpp
+++ Arduino/BlueCube/FaceSweep.cpp
@@ -1,41 +1,65 @@
 /*
- * FaceSweep.cpp - FaceSweep pattern for the Freetronics 4x4x4 Cube
+ *  File:     FaceSweep.cpp - FaceSweep pattern for the Freetronics 4x4x4 Cube (non blocking)
+ *  Version:  0.8
+ *  Author:   Adam Reed (adam@secretcode.ninja)
+ *  Licence:  BSD 3-Clause Licence
  */
 
+// Include for Arduino Library
 #include "Arduino.h"
+
+// Include for Cube Library
 #include "Cube.h"
+
+// Include the header file for this class
 #include "FaceSweep.h"
 
 FaceSweep::FaceSweep(Cube cube, int theDelay, rgb_t theColour)
 {
-  _theDelay = theDelay;
+  // Retain the reference to the cube
   _cube = cube;
+
+  // Retain the delay we will use
+  _theDelay = theDelay;
+
+  // Retain the colour to use
   _theColour = theColour;
 
+  // Set the default initial state for the animation
   _state = 1;
+
+  // Set the time we last ran the code to zero as it hasn't run yet
   _previousMillis = 0;
 }
 
 void FaceSweep::update()
 {
-  // check to see if it's time to change the state of the LED
+  // Handles drawing the FaceSweep animation.
+
+  /* This code is designed to be non blocking, so instead of using
+   * "delay()", it uses a state machine tp track where it is upto in the
+   * animation. It then uses the time and the difference between this run
+   * and the last run to determine if it needs to change to a different
+   * state
+   */
+
+  // Get the current time
   unsigned long currentMillis = millis();
 
   if ((_state == 1) && (currentMillis - _previousMillis >= _theDelay))
   {
-    _state = 2;  // Move to next state
-    _previousMillis = currentMillis;  // Remember the time
-
-    // Move 1
+    // Draw frame 1 of the animation
     _cube.all(BLACK);
     _cube.setplane(Y, 0, _theColour);
+
+    // Flag that we need to move to the next state
+    _state = 2;
+    // Remember the time for future reference
+    _previousMillis = currentMillis;
   }
   else if ((_state == 2) && (currentMillis - _previousMillis >= _theDelay))
   {
-    _state = 3;  // Move to next state
-    _previousMillis = currentMillis;  // Remember the time
-
-    // Move 2
+    // Draw frame 2 of the animation
     _cube.set(0, 0, 0, BLACK);
     _cube.set(1, 0, 0, BLACK);
     _cube.set(2, 0, 0, BLACK);
@@ -44,13 +68,15 @@ void FaceSweep::update()
     _cube.set(1, 1, 0, _theColour);
     _cube.set(2, 1, 0, _theColour);
     _cube.set(3, 1, 0, _theColour);
+
+    // Flag that we need to move to the next state
+    _state = 3;
+    // Remember the time for future reference
+    _previousMillis = currentMillis;
   }
   else if ((_state == 3) && (currentMillis - _previousMillis >= _theDelay))
   {
-    _state = 4;  // Move to next state
-    _previousMillis = currentMillis;  // Remember the time
-
-    // Move 3
+    // Draw frame 3 of the animation
     _cube.set(0, 1, 0, BLACK);
     _cube.set(1, 1, 0, BLACK);
     _cube.set(2, 1, 0, BLACK);
@@ -67,13 +93,15 @@ void FaceSweep::update()
     _cube.set(1, 1, 1, _theColour);
     _cube.set(2, 1, 1, _theColour);
     _cube.set(3, 1, 1, _theColour);
+
+    // Flag that we need to move to the next state
+    _state = 4;
+    // Remember the time for future reference
+    _previousMillis = currentMillis;
   }
   else if ((_state == 4) && (currentMillis - _previousMillis >= _theDelay))
   {
-    _state = 5;  // Move to next state
-    _previousMillis = currentMillis;  // Remember the time
-
-    // Move 4
+    // Draw frame 4 of the animation
     _cube.set(0, 2, 0, BLACK);
     _cube.set(1, 2, 0, BLACK);
     _cube.set(2, 2, 0, BLACK);
@@ -98,13 +126,15 @@ void FaceSweep::update()
     _cube.set(1, 1, 2, _theColour);
     _cube.set(2, 1, 2, _theColour);
     _cube.set(3, 1, 2, _theColour);
+
+    // Flag that we need to move to the next state
+    _state = 5;
+    // Remember the time for future reference
+    _previousMillis = currentMillis;
   }
   else if ((_state == 5) && (currentMillis - _previousMillis >= _theDelay))
   {
-    _state = 6;  // Move to next state
-    _previousMillis = currentMillis;  // Remember the time
-
-    // Move 5
+    // Draw frame 5 of the animation
     _cube.set(0, 3, 0, BLACK);
     _cube.set(1, 3, 0, BLACK);
     _cube.set(2, 3, 0, BLACK);
@@ -129,13 +159,15 @@ void FaceSweep::update()
     _cube.set(1, 3, 1, _theColour);
     _cube.set(2, 3, 1, _theColour);
     _cube.set(3, 3, 1, _theColour);
+
+    // Flag that we need to move to the next state
+    _state = 6;
+    // Remember the time for future reference
+    _previousMillis = currentMillis;
   }
   else if ((_state == 6) && (currentMillis - _previousMillis >= _theDelay))
   {
-    _state = 7;  // Move to next state
-    _previousMillis = currentMillis;  // Remember the time
-
-    // Move 6
+    // Draw frame 6 of the animation
     _cube.set(0, 2, 2, BLACK);
     _cube.set(1, 2, 2, BLACK);
     _cube.set(2, 2, 2, BLACK);
@@ -152,13 +184,15 @@ void FaceSweep::update()
     _cube.set(1, 3, 2, _theColour);
     _cube.set(2, 3, 2, _theColour);
     _cube.set(3, 3, 2, _theColour);
+
+    // Flag that we need to move to the next state
+    _state = 7;
+    // Remember the time for future reference
+    _previousMillis = currentMillis;
   }
   else if ((_state == 7) && (currentMillis - _previousMillis >= _theDelay))
   {
-    _state = 8;  // Move to next state
-    _previousMillis = currentMillis;  // Remember the time
-
-    // Move 7
+    // Draw frame 7 of the animation
     _cube.set(0, 3, 2, BLACK);
     _cube.set(1, 3, 2, BLACK);
     _cube.set(2, 3, 2, BLACK);
@@ -167,11 +201,20 @@ void FaceSweep::update()
     _cube.set(1, 3, 3, _theColour);
     _cube.set(2, 3, 3, _theColour);
     _cube.set(3, 3, 3, _theColour);
+
+    // Flag that we need to move to the next state
+    _state = 8;
+    // Remember the time for future reference
+    _previousMillis = currentMillis;
   }
   else if ((_state == 8) && (currentMillis - _previousMillis >= _theDelay * 5))
   {
-    _state = 1;  // Move to start
-    _previousMillis = currentMillis;  // Remember the time
+    // Draw frame 8 of the animation
+    // - This is the end state for the animation, so we "pause" for longer than
+    //   normal, then go back to the begining, remembering the time for future
+    //   reference
+    _state = 1;
+    _previousMillis = currentMillis;
   }
 }
 
--- Arduino/BlueCube/FaceSweep.h
+++ Arduino/BlueCube/FaceSweep.h
@@ -1,22 +1,46 @@
 /*
- * FaceSweep.h - Face Sweep pattern for the Freetronics 4x4x4 Cube
+ *  File:     FaceSweep.h - FaceSweep pattern for the Freetronics 4x4x4 Cube (non blocking)
+ *  Version:  0.8
+ *  Author:   Adam Reed (adam@secretcode.ninja)
+ *  Licence:  BSD 3-Clause Licence
  */
+
+// Ensure that this header file is only processed once, regardless of how
+// many time it is included
 #ifndef FaceSweep_h
 #define FaceSweep_h
 
+// Include for Arduino Library
 #include "Arduino.h"
+
+// Include for Cube Library
 #include "Cube.h"
 
+// Declare out class, with public and private variables and functions
 class FaceSweep {
   public:
-    FaceSweep(Cube cube,int theDelay, rgb_t theColour = BLUE);
-    void update(); 
+    // Constructor method, requiring the cube class, the time in
+    // milliseconds between animations, and the colour to use.
+    FaceSweep(Cube cube, int theDelay, rgb_t theColour = BLUE);
+
+    // Function to determine what the current state of the annimation 
+    // should be, and to update the cube to that state if appropriate.
+    void update();
 
   private:
-    int _theDelay;
+    // Reference to the cube  
     Cube _cube;
+
+    // Delay between animation states in milliseconds
+    int _theDelay;
+
+    // Colour to use
     rgb_t _theColour;
+
+    // The state the animation is in
     int _state;
+
+    // Tracks the time the code was last run
     unsigned long _previousMillis;
 };
 
