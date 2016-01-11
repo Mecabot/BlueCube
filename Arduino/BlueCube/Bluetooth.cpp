c68b68d8afa65b11a0a1916a461a1b60ebfbff9a
Adam Reed <adam@secretcode.ninja>
2016-01-11 12:14:02 +1100

    Improve the comments in the bluetooth class, remove an unused variable, and comment out the printing of the characters we receive.

--- Arduino/BlueCube/Bluetooth.cpp
+++ Arduino/BlueCube/Bluetooth.cpp
@@ -1,37 +1,55 @@
 /*
- * Bluetooth.cpp - Bluetooth functionality for Freetronics 4x4x4 Cube
+ *  File:     Bluetooth.cpp - Non blocking bluetooth functionality 
+ *            for Freetronics 4x4x4 Cube
+ *  Version:  0.8
+ *  Author:   Adam Reed (adam@secretcode.ninja)
+ *  Licence:  BSD 3-Clause Licence
  */
 
+// Include for Arduino Library
 #include "Arduino.h"
+
+// Include for Cube Library
 #include "Cube.h"
+
+// Include for Bluetooth Hardware
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_UART.h"
+
+// Include the header file for this class
 #include "Bluetooth.h"
 
 Bluetooth::Bluetooth(Adafruit_BLE *ble, int timeout)
 {
+  // Retain a pointer to the BLE object for use within this class
   _ble = ble;
-  _timeout = timeout;
-  _previousMillis = 0;
 
+  // Retain the timeout to use
+  _timeout = timeout;
+
+  // Call the prepare function to set other defaults
   _prepareForCommand();
 }
 
 void Bluetooth::_prepareForCommand()
 {
+  // Called once at the start of each attempt to read a command
+  
   // Reset the packet buffer
   memset(_packetBuffer, 0, READ_BUFSIZE + 1);   // Fill buffer with zeros
-  _bufferIndex = 0;                             // Current pos within the received char buffer
+  _bufferIndex = 0;                             // Reset current position within the  
+                                                // received character buffer
+  // Reset the time remaining before timeout
   _timeRemaining = _timeout;
 }
 
 void Bluetooth::checkForCommand()
 {
   if (_timeRemaining >= 0) {
-    // Loop whilever we haven't reached the timeout
+    // Process code while we haven't reached the timeout
 
     if (_bufferIndex >= READ_BUFSIZE) {
-      // Exit the loop if the buffer we have allocated is full
+      // If the buffer we have allocated is full, stop the time remaining countdown
       _timeRemaining = 0;
     } else {
 
@@ -46,7 +64,7 @@ void Bluetooth::checkForCommand()
         _bufferIndex++;
 
         // Print the character we have received to the USB serial output
-        serial->print(c);
+        //serial->print(c);
 
         // Reset the timeout counter to our original timeout value
         _timeRemaining = _timeout;
@@ -54,8 +72,8 @@ void Bluetooth::checkForCommand()
         // Look for the character we use to indicate the end of a message
         if (c == ';') {
           // We have reached the end of the message, so print a new line
-          serial->println();
-
+          //serial->println();
+          
           // Set the timeout to zero as we don't need to wait any longer for characters
           _timeRemaining = 0;
 
@@ -68,16 +86,26 @@ void Bluetooth::checkForCommand()
   
   if (_timeRemaining == 0 )
   {
-    _packetBuffer[_bufferIndex] = 0;  // null term
-
     if (!_bufferIndex) {
+      // no data or we hit the timeout
+
+      // Reset the buffer
       _prepareForCommand();
-      return;               // no data or we hit the timeout, so exit the function
+
+      // exit the function
+      return;               
     }
 
     // We have data, so pass it to the cube's parser to read the message and act upon it
+    
+    // Null terminate the packet buffer
+    _packetBuffer[_bufferIndex] = 0;  
+
+    // Call the cube libraries parser to action the message
     bytecode_t bytecode = {};
     byte errorCode = parser(_packetBuffer, sizeof(_packetBuffer), & bytecode);
+
+    // Reset the buffer
     _prepareForCommand();
   }
 }
--- Arduino/BlueCube/Bluetooth.h
+++ Arduino/BlueCube/Bluetooth.h
@@ -1,34 +1,62 @@
 /*
- * Bluetooth.h - Bluetooth functionality for Freetronics 4x4x4 Cube
+ *  File:     Bluetooth.h - Non blocking bluetooth functionality 
+ *            for Freetronics 4x4x4 Cube
+ *  Version:  0.8
+ *  Author:   Adam Reed (adam@secretcode.ninja)
+ *  Licence:  BSD 3-Clause Licence
  */
+
+// Ensure that this header file is only processed once, regardless of how
+// many time it is included
 #ifndef Bluetooth_h
 #define Bluetooth_h
 
+/*==========================================================================
+    APPLICATION SETTINGS (USER EDITABLE)
+
+    BLE_READPACKET_TIMEOUT    Timeout in ms waiting to read a message
+    READ_BUFSIZE              Size of the read buffer for incoming packets
+============================================================================ */
+#define BLE_READPACKET_TIMEOUT         500
+#define READ_BUFSIZE                   32
+/*========================================================================== */
+
+// Include for Arduino Library
 #include "Arduino.h"
+
+// Include for Cube Library
 #include "Cube.h"
+
+// Include for Bluetooth Hardware
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_UART.h"
 
-// COMMON SETTINGS
-// ----------------------------------------------------------------------------------------------
-// These settings are used in both SW UART, HW UART and SPI mode
-// ----------------------------------------------------------------------------------------------
-#define BLE_READPACKET_TIMEOUT         500    // Timeout in ms waiting to read a response
-#define READ_BUFSIZE                   32     // Size of the read buffer for incoming packets
-
+// Declare out class, with public and private variables
 class Bluetooth {
   public:
+    // Constructor method, requiring a pointer to the bluetooth object, and 
+    // the time in milliseconds to wait for a complete message
     Bluetooth(Adafruit_BLE *ble, int timeout);
+
+    // Function for checking and processing any received messages
     void checkForCommand(); 
 
   private:
+    // Bluetooth object
     Adafruit_BLE *_ble;
+
+    // Maximum time to wait for a message to be received
     int _timeout;
+
+    // Time remaining to wait for the message to be completely received
     int _timeRemaining;
+
+    // Buffer that holds the recieved message, and the index for where
+    // the next character should be inserted
     char _packetBuffer[READ_BUFSIZE + 1];
     int _bufferIndex;
-    unsigned long _previousMillis;
 
+    // Function to reset items between attempts to receive a message
     void _prepareForCommand();
 };
 
